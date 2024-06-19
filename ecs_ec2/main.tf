0cd6222ac0db43c26b9e9c63e6716bdd12fb62c3
Goutham Pratapa <goutham@appsmith.com>
2024-06-19 16:11:31 +0530

    feat: add terraform modules for ecs-ec2

---
+++ ecs_ec2/main.tf
@@ -0,0 +1,247 @@
+## Ecs-node Related
+data "aws_iam_policy_document" "ecs_node_doc" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_node_role" {
+  name_prefix        = "appsmith-ecs-node-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_node_doc.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_node_container_service_policy" {
+  role       = aws_iam_role.ecs_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_node_container_policy" {
+  role       = aws_iam_role.ecs_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_node_ebs_driver_policy" {
+  role       = aws_iam_role.ecs_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+resource "aws_iam_instance_profile" "ecs_node" {
+  name_prefix = "appsmith-ecs-node-profile"
+  path        = "/ecs/instance/"
+  role        = aws_iam_role.ecs_node_role.name
+}
+
+## Cluster Related
+resource "aws_ecs_cluster" "main" {
+  name = "appsmith-cluster"
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+  tags = {
+    Name = "appsmith"
+  }
+}
+
+resource "aws_security_group" "ecs_node_sg" {
+  name        = "appsmith-security-group"
+  vpc_id      = var.vpc_id
+  description = "Security groups rules to apply"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "appsmith"
+  }
+}
+
+data "aws_ssm_parameter" "ecs_node_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+}
+
+resource "aws_launch_template" "ecs_ec2" {
+  name_prefix            = "appsmith-"
+  image_id               = data.aws_ssm_parameter.ecs_node_ami.value
+  instance_type          = var.ecs_instance_type
+  vpc_security_group_ids = [aws_security_group.ecs_node_sg.id]
+  iam_instance_profile { arn = aws_iam_instance_profile.ecs_node.arn }
+  monitoring { enabled = true }
+  user_data = base64encode(<<-EOF
+      #!/bin/bash
+      echo ECS_CLUSTER=${aws_ecs_cluster.main.name} >> /etc/ecs/ecs.config;
+    EOF
+  )
+}
+
+resource "aws_autoscaling_group" "ecs" {
+  name_prefix               = "appsmith-ecs-asg-"
+  vpc_zone_identifier       = var.ecs_subnet_id
+  min_size                  = 1
+  max_size                  = 2
+  health_check_grace_period = 0
+  health_check_type         = "EC2"
+
+  launch_template {
+    id      = aws_launch_template.ecs_ec2.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "appsmith-ecs-cluster"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = ""
+    propagate_at_launch = true
+  }
+}
+
+
+resource "aws_ecs_capacity_provider" "main" {
+  name = "appsmith-ecs-ec2"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.ecs.arn
+    managed_termination_protection = "DISABLED"
+
+    managed_scaling {
+      maximum_scaling_step_size = 2
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 100
+    }
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name       = aws_ecs_cluster.main.name
+  capacity_providers = [aws_ecs_capacity_provider.main.name]
+
+  default_capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.main.name
+    base              = 1
+    weight            = 100
+  }
+}
+
+
+## Task Related
+
+data "aws_iam_policy_document" "ecs_task_doc" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_exec_role" {
+  name_prefix        = "appsmith-ecs-exec-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec_role_policy" {
+  role       = aws_iam_role.ecs_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_secret_policy" {
+  role       = aws_iam_role.ecs_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
+
+resource "aws_cloudwatch_log_group" "ecs" {
+  name              = "/appsmith-ecs/demo"
+  retention_in_days = 14
+}
+
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+  family                   = "appsmith-task-definition"
+  requires_compatibilities = ["EC2"]
+  task_role_arn            = aws_iam_role.ecs_exec_role.arn
+  execution_role_arn       = aws_iam_role.ecs_exec_role.arn
+  cpu                      = 1024
+  memory                   = 3072
+  tags                     = { Name = "appsmith" }
+  volume {
+    name      = "appsmith"
+    host_path = "/appsmith-stacks"
+  }
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+  container_definitions = jsonencode([{
+    name      = "appsmith-task-definition",
+    image     = var.appsmith_image
+    essential = true
+    portMappings = [
+      { containerPort = 80, hostPort = 80 },
+      { containerPort = 443, hostPort = 443 }
+    ]
+    healthCheck = {
+      command = [
+        "CMD-SHELL",
+        "curl http://localhost/api/v1/health || exit 1"
+      ]
+    }
+    logConfiguration = {
+      logDriver = "awslogs",
+      options = {
+        "awslogs-region"        = var.region,
+        "awslogs-group"         = aws_cloudwatch_log_group.ecs.name,
+        "awslogs-stream-prefix" = "appsmith"
+      }
+    }
+    mountPoints = [{ sourceVolume = "appsmith", containerPath = "/appsmith-stacks", "readOnly" = false }]
+    environment = [
+      { name = "APPSMITH_ENCRYPTION_PASSWORD", value = "La01QBbbtFgQa" },
+      { name = "APPSMITH_ENCRYPTION_SALT", value = "xGkMZcJRCB3zS" },
+      { name = "APPSMITH_ENABLE_EMBEDDED_DB", value = "0" }
+    ]
+  }])
+}
+
+# ## ECS service
+
+resource "aws_ecs_service" "appsmith_ecs_service" {
+  name            = "appsmith"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+  launch_type     = "EC2"
+  desired_count   = 1
+}
+
