7bac0b16a8e4692a594dd51e14878dc585fef8b2
Goutham Pratapa <goutham@appsmith.com>
2024-08-17 20:57:54 +0530

    update: fargate installation docs

--- aws/ecs_fargate/main.tf
+++ aws/ecs_fargate/main.tf
@@ -9,25 +9,54 @@ resource "aws_ecs_cluster" "main" {
   }
 }
 
-
-data "aws_iam_policy_document" "ecs_task_doc" {
+data "aws_iam_policy_document" "ssm_policy_doc" {
   statement {
-    actions = [ "ssmmessages:CreateControlChannel",
-                "ssmmessages:CreateDataChannel",
-                "ssmmessages:OpenControlChannel",
-                "ssmmessages:OpenDataChannel"]
-    effect  = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
+    effect = "Allow"
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+    "ssmmessages:OpenDataChannel"]
+    resources = ["*"]
   }
 }
 
+resource "aws_iam_policy" "ssm_policy" {
+  name_prefix = "ssm-policy"
+  description = "Policy to exec into fargate containers"
+  policy      = data.aws_iam_policy_document.ssm_policy_doc.json
+}
+
 resource "aws_iam_role" "ecs_exec_role" {
-  name_prefix        = "appsmith-ecs-exec-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_doc.json
+  name_prefix = "appsmith-ecs-exec-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec_role_policy" {
+  role       = aws_iam_role.ecs_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "role-policy-attach" {
+  role       = aws_iam_role.ecs_exec_role.name
+  policy_arn = aws_iam_policy.ssm_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_secret_policy" {
+  role       = aws_iam_role.ecs_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
 
 ### ecs fargate sg
@@ -47,6 +76,13 @@ resource "aws_security_group" "ecs_sg" {
     protocol    = "tcp"
     from_port   = 443
     to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 2049
+    to_port     = 2049
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -70,6 +106,25 @@ resource "aws_cloudwatch_log_group" "ecs_cloudwatch_log_group" {
 }
 
 ### EFS
+resource "aws_efs_file_system" "efs" {
+  creation_token   = "appsmith-efs"
+  performance_mode = "generalPurpose"
+  throughput_mode  = "bursting"
+  encrypted        = "true"
+  tags = {
+    Name = "appsmith"
+  }
+}
+
+## mount target
+resource "aws_efs_mount_target" "efs-mount-targets" {
+  count           = var.ecs_subnet_count
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = var.ecs_subnet_id[count.index]
+  security_groups = [aws_security_group.efs_sg.id]
+}
+
+
 resource "aws_security_group" "efs_sg" {
   name        = "appsmith-efs-security-group"
   vpc_id      = var.vpc_id
@@ -95,41 +150,29 @@ resource "aws_security_group" "efs_sg" {
 
 }
 
-resource "aws_efs_file_system" "efs" {
-  creation_token   = "appsmith-efs"
-  performance_mode = "generalPurpose"
-  throughput_mode  = "bursting"
-  encrypted        = "true"
-  tags = {
-    Name = "appsmith"
-  }
-}
-
-## mount target
-resource "aws_efs_mount_target" "efs-mount-targets" {
-  count           = var.ecs_subnet_count
-  file_system_id  = aws_efs_file_system.efs.id
-  subnet_id       = var.ecs_subnet_id[count.index]
-  security_groups = [aws_security_group.efs_sg.id]
-}
-
 ## ECS service
 
 resource "aws_ecs_service" "appsmith_ecs_service" {
   name            = "appsmith"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
-  launch_type     = "EC2"
+  launch_type     = "FARGATE"
   desired_count   = 1
+  network_configuration {
+    subnets         = var.ecs_subnet_id
+    security_groups = [aws_security_group.ecs_sg.id, aws_security_group.efs_sg.id]
+    assign_public_ip = true
+  }  
 }
 
 resource "aws_ecs_task_definition" "ecs_task_definition" {
   family                   = "appsmith-task-definition"
   requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
   task_role_arn            = aws_iam_role.ecs_exec_role.arn
   execution_role_arn       = aws_iam_role.ecs_exec_role.arn
-  cpu                      = 1024
-  memory                   = 3072
+  cpu                      = 2048
+  memory                   = 4096
   tags                     = { Name = "appsmith" }
   volume {
     name = "appsmith-efs"
